8932645cb3904f8f885114eb219a78879b23db85
Madusanka Dissanayake <mdissanayake994@gmail.com>
2024-11-04 08:41:27 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import styles from "@/app/ui/home.module.css";
+import { lusitana } from "@/app/ui/fonts";
+import Image from "next/image";
+
+export default function Page() {
+	return <>Hi</>;
+}
