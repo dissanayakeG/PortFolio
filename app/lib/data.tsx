c65729a23d1919c476a87c577410527d0f53731f
Madusanka Dissanayake <mdissanayake994@gmail.com>
2024-11-16 13:31:52 +0530

    ui updates

--- app/lib/data.tsx
+++ app/lib/data.tsx
@@ -4,47 +4,49 @@ const experiences: Experience[] = [
 	{
 		company: "OpusXenta",
 		duration: "July 2019 - November 2022",
-		skills: "PHP | JavaScript | Laravel | VueJs | MySql",
+		skills: "PHP | JavaScript | Laravel | Vue.Js | MySQL",
 		breakdown: [
 			{
 				designation: "Trainee Software Engineer",
 				duration: "July 2019 - December 2019",
-				notes: "Focused in mastering Laravel framework and VueJs Framework",
+				notes: "Gained hands-on experience with the Laravel and Vue.js frameworks, focusing on backend and frontend integration.",
 			},
 			{
 				designation: "Associate Software Engineer",
 				duration: "January 2020 - May 2020",
-				notes: `Created and implemented complex VueJs components |
-						Integrated google map and Google API`,
+				notes: `Developed and integrated advanced Vue.js components, enhancing user interfaces and user experiences. |
+						Successfully implemented Google Maps and Google API integration to add geolocation and mapping functionalities.`,
 			},
 			{
 				designation: "Software Engineer",
 				duration: "May 2020 - August 2022",
-				notes: `Designed, modified and implemented technology solutions |
-						Created and implemented complex VueJs components | 
-						Designed complex database designs | 
-						Lead a Team`,
+				notes: `Designed and developed scalable technology solutions to meet complex client requirements. |
+						Created and implemented advanced Vue.js components, optimizing performance and functionality. | 
+						Architected and managed complex database structures for high-performance applications. | 
+						Led a cross-functional team, fostering collaboration and delivering projects on time.`,
 			},
 			{
 				designation: "Senior Software Engineer",
 				duration: "August 2022 - November 2022",
-				notes: `Designed, modified and implemented technology solutions | 
-						Designed complex database designing | 
-						Lead a Team`,
+				notes: `Led the design and development of robust technology solutions, ensuring scalability and maintainability. | 
+						Spearheaded database architecture for high-demand applications, optimizing for speed and reliability. | 
+						Mentored team members and managed project workflows to ensure high-quality deliverables.
+`,
 			},
 		],
 	},
 	{
 		company: "Intervest",
 		duration: "November 2022 - Present",
-		skills: "Laravel | Laravel Package Development | Livewire | AlpineJs | Pest | TDD",
+		skills: "Laravel | Laravel Package Development | Livewire | Alpine.js | Pest | TDD",
 		breakdown: [
 			{
 				designation: "Senior Software Engineer",
 				duration: "November 2022 - Present",
-				notes: `Laravel Package developments |
-						Using design patterns to develop expandable codes |
-						Using PHPUnit and Pest for TDD`,
+				notes: `Developed custom Laravel packages to streamline application functionality and reusability. |
+						Applied design patterns to create modular, maintainable, and expandable codebases. |
+						Led test-driven development (TDD) using PHPUnit and Pest, ensuring robust and error-free applications. | 
+						Collaborated with cross-functional teams to implement innovative solutions and optimize performance.`,
 			},
 		],
 	},
--- app/ui/partials/experienceCard.tsx
+++ app/ui/partials/experienceCard.tsx
@@ -5,40 +5,46 @@ type ExperienceType = {
 };
 
 export default function ExperienceCard({ experience }: ExperienceType) {
-	const card = (
-		<div>
-			<div className="flex flex-col sm:flex-row justify-between text-secondary-light text-xl italic">
-				<h1>{experience.company}</h1>
-				<h1>{experience.duration}</h1>
+	const renderNotes = (notes: string) => {
+		return notes
+			.split("|")
+			.map((segment, index) => <div key={index}>{segment}</div>);
+	};
+
+	const renderBreakdown = () =>
+		experience.breakdown?.map((data) => (
+			<div key={data.duration} className="mb-4">
+				<div className="flex flex-col sm:flex-row justify-between">
+					<h1 className="text-secondary-light">{data.designation}</h1>
+					<h1 className="text-secondary-light">{data.duration}</h1>
+				</div>
+
+				{/* The && operator is used to check if data.notes is truthy (not null, undefined, false, 0, NaN, or an empty string). */}
+
+				{data.notes && (
+					<h1 className="ml-2 text-sm">{renderNotes(data.notes)}</h1>
+				)}
 			</div>
-			<p className="text-white italic text-md font-bold">
-				{experience.skills.toUpperCase()}
-			</p>
+		));
 
-			<div className="w-full h-0.5 bg-theme-light"></div>
-
+	return (
+		<div className="w-full">
 			<div>
-				{experience.breakdown
-					? experience.breakdown.map((data) => {
-							return (
-								<div key={data.duration}>
-									<div className="flex flex-col sm:flex-row justify-between">
-										<h1 className="text-secondary-light">
-											{data.designation}
-										</h1>
-										<h1 className="text-secondary-light">
-											{data.duration}
-										</h1>
-									</div>
-									<h1 className="ml-2">{data.notes}</h1>
-								</div>
-							);
-					  })
-					: ""}
+				<div className="flex flex-col sm:flex-row justify-between text-secondary-light text-xl italic">
+					<h1>{experience.company}</h1>
+					<h1>{experience.duration}</h1>
+				</div>
+
+				<p className="text-white italic text-md font-bold">
+					{experience.skills.toUpperCase()}
+				</p>
+
+				<div className="w-full h-0.5 bg-theme-light my-4"></div>
+
+				<div>{renderBreakdown()}</div>
+
+				<div className="w-full h-0.5 bg-theme my-4"></div>
 			</div>
-			<div className="w-full h-0.5 bg-theme"></div>
 		</div>
 	);
-
-	return <div className="w-full">{card}</div>;
 }
