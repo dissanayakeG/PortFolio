0ed8cf18ffdb1e4113efa70dce90455e26e97396
Madusanka Dissanayake <mdissanayake994@gmail.com>
2024-12-21 16:03:35 +0530

    updates

--- app/taxCalculator/page.tsx
+++ app/taxCalculator/page.tsx
@@ -30,8 +30,7 @@ export default function Page() {
 	};
 
 	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const value = e.target.value;
-		// Ensure the value is a valid number
+		const value = e.target.value.replace(/\D/g, "");
 		const parsedValue = value ? parseInt(value) : 0;
 		setYourSalary(parsedValue);
 	};
@@ -40,7 +39,8 @@ export default function Page() {
 		setTotalTax(0);
 		if (yourSalary > 10_000_000) {
 			setTotalTax(
-				"Don't consider your tax, if you get this much salary..."
+				`Don't Lie😂
+				Maximum calculation limit is 10 million😎`
 			);
 			return;
 		}
@@ -54,32 +54,62 @@ export default function Page() {
 	};
 
 	return (
-		<div className="w-full max-w-xs">
-			<form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+		<div className="w-full min-w-md max-w-md">
+			<form className="bg-primary-dark shadow-md rounded px-8 pt-6 pb-8 mb-4 min-w-80">
 				<div className="mb-4">
 					<label
 						className="block text-gray-700 text-sm font-bold mb-2"
 						htmlFor="salary"
 					>
-						Your Salary
+						Enter Your Salary
 					</label>
 					<input
-						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+						className="min-w-md max-w-md shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 						id="salary"
 						type="text"
 						placeholder="Enter Salary"
-						onBlur={handleInputChange}
+						onChange={handleInputChange}
+						value={yourSalary}
 					/>
 				</div>
-				<p>Your Total Tax: {totalTax}</p>
 
 				<button
-					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+					className="shadow bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full max-w-md"
 					type="button"
 					onClick={calculateFinalTax}
 				>
 					Calculate
 				</button>
+				{typeof totalTax === "number" ? (
+					<>
+						<div className="mt-2 font-bold flex">
+							<p className="text-secondary-light mr-2">
+								Your Total Tax is (LKR):
+							</p>
+							<p className="text-red-500">{totalTax}</p>
+						</div>
+						<div className="mt-2 font-bold flex">
+							<p className="text-secondary-light mr-2">
+								Remaining Salary (LKR):
+							</p>
+							<p className="text-green-500">
+								{yourSalary - totalTax}
+							</p>
+						</div>
+						{totalTax ? (
+							<div className="text-green-500">
+								Tax percentage for total salary :{" "}
+								{((totalTax / yourSalary) * 100).toFixed(2)} %
+							</div>
+						) : (
+							<></>
+						)}
+					</>
+				) : (
+					<p className="mt-2 font-normal text-secondary">
+						{totalTax}
+					</p>
+				)}
 			</form>
 		</div>
 	);
