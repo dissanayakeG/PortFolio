508f17f51f95f674dfc60d41138872f7acd80cd6
Madusanka Dissanayake <mdissanayake994@gmail.com>
2025-03-17 14:57:58 +0530

    changes

---
+++ app/api/posts/route.ts
@@ -0,0 +1,8 @@
+// app/api/posts/route.ts
+import { NextResponse } from "next/server";
+import { getPostMetaDataTags } from "@/app/lib/getPostMetaData";
+
+export async function GET() {
+    const posts = getPostMetaDataTags();
+    return NextResponse.json(posts);
+}
