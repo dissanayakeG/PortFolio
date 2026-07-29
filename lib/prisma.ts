a01858bf99675d7b0b2c8dcdfed83b0e3d39758a
Madusanka Dissanayake <mdissanayake994@gmail.com>
2026-07-29 19:26:28 +0530

    chore: pipeline update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,12 @@
 import { PrismaClient } from "@/generated/prisma";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
+// const adapter = new PrismaBetterSqlite3({
+//   url: "./study.db",
+// });
+
 const adapter = new PrismaBetterSqlite3({
-  url: "./study.db",
+  url: process.env.DATABASE_URL!.replace("file:", ""),
 });
 
 const globalForPrisma = globalThis as unknown as {
